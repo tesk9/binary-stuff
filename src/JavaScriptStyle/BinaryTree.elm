4d6beba5dd4dd5a09ee5302ec3d223c0d753ca31
Tessa Kelly <tessakkelly@gmail.com>
2016-09-04 20:34:12 -0700

    adds insert

--- src/JavaScriptStyle/BinaryTree.elm
+++ src/JavaScriptStyle/BinaryTree.elm
@@ -3,6 +3,7 @@ module JavaScriptStyle.BinaryTree
         ( BinaryTree
         , new
         , member
+        , insert
         )
 
 import Maybe.Extra
@@ -33,3 +34,21 @@ member value (BinaryTree tree) =
         Maybe.Extra.mapDefault False (member value) tree.right
     else
         value == tree.value
+
+
+insert : comparable -> BinaryTree comparable -> BinaryTree comparable
+insert value (BinaryTree tree) =
+    if value < tree.value then
+        BinaryTree
+            { value = tree.value
+            , left = Just <| Maybe.Extra.mapDefault (new value) (insert value) tree.left
+            , right = tree.right
+            }
+    else if value > tree.value then
+        BinaryTree
+            { value = tree.value
+            , left = tree.left
+            , right = Just <| Maybe.Extra.mapDefault (new value) (insert value) tree.right
+            }
+    else
+        BinaryTree tree
