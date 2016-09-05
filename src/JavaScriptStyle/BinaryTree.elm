567311535ff5afc88d50d279f89365bc7dfd5e6b
Tessa Kelly <tessakkelly@gmail.com>
2016-09-04 21:37:26 -0700

    Refactor to allow removal to be possible

--- src/JavaScriptStyle/BinaryTree.elm
+++ src/JavaScriptStyle/BinaryTree.elm
@@ -2,59 +2,108 @@ module JavaScriptStyle.BinaryTree
     exposing
         ( BinaryTree
         , new
+        , empty
         , member
         , insert
         , remove
         )
 
-import Maybe.Extra
-
 
 type BinaryTree comparable
-    = BinaryTree
+    = Node
         { value : comparable
-        , left : Maybe (BinaryTree comparable)
-        , right : Maybe (BinaryTree comparable)
+        , left : BinaryTree comparable
+        , right : BinaryTree comparable
         }
+    | Empty
+
+
+empty : BinaryTree comparable
+empty =
+    Empty
 
 
 new : comparable -> BinaryTree comparable
 new value =
-    BinaryTree
+    Node
         { value = value
-        , left = Nothing
-        , right = Nothing
+        , left = Empty
+        , right = Empty
         }
 
 
 member : comparable -> BinaryTree comparable -> Bool
-member value (BinaryTree tree) =
-    if value < tree.value then
-        Maybe.Extra.mapDefault False (member value) tree.left
-    else if value > tree.value then
-        Maybe.Extra.mapDefault False (member value) tree.right
-    else
-        value == tree.value
+member value tree =
+    case tree of
+        Node node ->
+            if value < node.value then
+                member value node.left
+            else if value > node.value then
+                member value node.right
+            else
+                value == node.value
+
+        Empty ->
+            False
 
 
 insert : comparable -> BinaryTree comparable -> BinaryTree comparable
-insert value (BinaryTree tree) =
-    if value < tree.value then
-        BinaryTree
-            { value = tree.value
-            , left = Just <| Maybe.Extra.mapDefault (new value) (insert value) tree.left
-            , right = tree.right
-            }
-    else if value > tree.value then
-        BinaryTree
-            { value = tree.value
-            , left = tree.left
-            , right = Just <| Maybe.Extra.mapDefault (new value) (insert value) tree.right
-            }
-    else
-        BinaryTree tree
+insert value tree =
+    case tree of
+        Node node ->
+            if value < node.value then
+                Node
+                    { value = node.value
+                    , left = insert value node.left
+                    , right = node.right
+                    }
+            else if value > node.value then
+                Node
+                    { value = node.value
+                    , left = node.left
+                    , right = insert value node.right
+                    }
+            else
+                Node node
+
+        Empty ->
+            new value
 
 
 remove : comparable -> BinaryTree comparable -> BinaryTree comparable
 remove value tree =
-    tree
+    case tree of
+        Node node ->
+            if value < node.value then
+                Node
+                    { value = node.value
+                    , left = remove value node.left
+                    , right = node.right
+                    }
+            else if value > node.value then
+                Node
+                    { value = node.value
+                    , left = node.left
+                    , right = remove value node.right
+                    }
+            else
+                case ( node.left, node.right ) of
+                    ( left, Node rightTree ) ->
+                        Node
+                            { value = rightTree.value
+                            , right = rightTree.right
+                            , left = left
+                            }
+
+                    ( Node leftTree, Empty ) ->
+                        Node
+                            { value = leftTree.value
+                            , right = empty
+                            , left = leftTree.left
+                            }
+
+                    ( Empty, Empty ) ->
+                        empty
+
+        Empty ->
+            Empty
